a88eb346f1fb4e807b0293a056a5d0071a0574ae
Jon Sousa <jonathonsousa@gmail.com>
2023-06-24 20:50:16 -0500

    Removed annoying text

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        console.log(`Ready!!! ${client.user.tag} is logged in and online!`);
+        console.log(`${client.user.tag} is logged in and online!`);
     }
 }
