2d9cda32708b64ce3e21e3fe2c536ea6512d2c8b
Jon Sousa <jonathonsousa@gmail.com>
2023-08-19 21:39:26 -0500

    jokes work now!

--- src/commands/tools/joke.js
+++ src/commands/tools/joke.js
@@ -1,18 +1,28 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('joke')
-    .setDescription('Return a button!'),
-    async execute(interaction, client) {
-        const button = new ButtonBuilder()
-        .setCustomId('joke')
-        // .setEmoji()
-        .setLabel('Tell me a joke!')
-        .setStyle(ButtonStyle.Success);
-
-        await interaction.reply({
-            components: [new ActionRowBuilder().addComponents(button)]
-        });
+    .setDescription('Returns with a joke!'),
+    async execute(interaction) {
+        const response = await fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit", {credentials:"include"});
+        const result = await response.json();
+        
+        if(result.joke) {
+            await interaction.reply({
+                content: `${result.joke}`
+            });
+        } else {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            const embed = new EmbedBuilder()
+            .setTitle(`${result.setup}`)
+            .setDescription(`${result.delivery}`)
+            .setColor(`${randomColor}`)
+            .setTimestamp(Date.now());
+                                
+            await interaction.reply({
+                embeds: [embed]
+            });
+        }
     }
 }
