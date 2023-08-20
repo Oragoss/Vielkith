f9348182d9e98855d70031d35a21d9a5ea00025e
Jon Sousa <jonathonsousa@gmail.com>
2023-08-19 21:44:03 -0500

    Added chuck norris jokes because reasons

--- src/commands/tools/joke.js
+++ src/commands/tools/joke.js
@@ -5,24 +5,34 @@ module.exports = {
     .setName('joke')
     .setDescription('Returns with a joke!'),
     async execute(interaction) {
-        const response = await fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit", {credentials:"include"});
-        const result = await response.json();
-        
-        if(result.joke) {
+        let randomInt = Math.floor(Math.random() * 50);
+        if(randomInt === 25) {
+            const response = await fetch("https://api.chucknorris.io/jokes/random", {credentials:"include"});
+            const result = await response.json();
+
             await interaction.reply({
-                content: `${result.joke}`
+                content: `${result.value}`
             });
         } else {
-            const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            const embed = new EmbedBuilder()
-            .setTitle(`${result.setup}`)
-            .setDescription(`${result.delivery}`)
-            .setColor(`${randomColor}`)
-            .setTimestamp(Date.now());
-                                
-            await interaction.reply({
-                embeds: [embed]
-            });
-        }
+            const response = await fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit", {credentials:"include"});
+            const result = await response.json();
+            
+            if(result.joke) {
+                await interaction.reply({
+                    content: `${result.joke}`
+                });
+            } else {
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                const embed = new EmbedBuilder()
+                .setTitle(`${result.setup}`)
+                .setDescription(`${result.delivery}`)
+                .setColor(`${randomColor}`)
+                .setTimestamp(Date.now());
+                                    
+                await interaction.reply({
+                    embeds: [embed]
+                });
+            }
+        }        
     }
 }
